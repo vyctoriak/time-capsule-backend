164725eabbe350081a5e00b3e7a263ef9c53aed9
Vyctoria Karina <vyctoriak@gmail.com>
2023-08-25 14:28:16 +0200

    install axios, create github auth component and getting user informations to create an account

---
+++ src/routes/auth.ts
@@ -0,0 +1,40 @@
+import { FastifyInstance } from 'fastify'
+import { z } from 'zod'
+import axios from 'axios'
+
+export async function authRoutes(app: FastifyInstance) {
+  app.post('/register', async (request) => {
+    const bodySchema = z.object({
+      code: z.string(),
+    })
+
+    const { code } = bodySchema.parse(request.body)
+
+    const accessTokenResponde = await axios.post(
+      'https://github.com/login/oauth/access_token',
+      null,
+      {
+        params: {
+          client_id: process.env.GITHUB_CLIENT_ID,
+          client_secret: process.env.GITHUB_CLIENT_SECRET,
+          code,
+        },
+        headers: {
+          Accept: 'application/json',
+        },
+      },
+    )
+
+    const { access_token } = accessTokenResponde.data
+
+    const userResponse = await axios.get('https://api.github.com/user', {
+      headers: {
+        Authorization: `Bearer ${access_token}`,
+      },
+    })
+
+    const user = userResponse.data
+
+    return { user }
+  })
+}
